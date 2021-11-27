a7031a7b627839eee637e49b994c197e6b21713d
aleksandrdiptan <diptan@huntflow.ru>
2021-11-27 17:09:50 +0300

    hw3_1

---
+++ hw3/3_1.py
@@ -0,0 +1,7 @@
+# 1. В диапазоне натуральных чисел от 2 до 99 определить, сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
+
+numb_range_big = [item for item in range(2, 99)]
+numb_range_small = [item for item in range(2, 9)]
+
+print(numb_range_big)
+print((numb_range_small))
