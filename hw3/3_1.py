7ab579f705ad4b92320e1784d65fbcd2974f8dc1
alexdiptan <diptan@google.com>
2021-11-27 17:41:43 +0300

    3_1 done

--- hw3/3_1.py
+++ hw3/3_1.py
@@ -3,5 +3,15 @@
 numb_range_big = [item for item in range(2, 99)]
 numb_range_small = [item for item in range(2, 9)]
 
-print(numb_range_big)
-print((numb_range_small))
+for number_small in numb_range_small:
+    numbers_count = 0
+    for number_big in numb_range_big:
+        if number_big % number_small == 0:
+            numbers_count += 1
+    print(f'Число {number_small} - кратно для {numbers_count} чисел из большого списка.')
+
+print('--------------------------------------------------------')
+# или второй вариант решения:
+for number_small in numb_range_small:
+    print(f'Число {number_small} - кратно для {len([x for x in numb_range_big if x % number_small == 0])} '
+          f'чисел из большого списка.')
