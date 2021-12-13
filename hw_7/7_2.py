368af5452238c943299db36efadf7f2325f5c28b
aleksandrdiptan <diptan@huntflow.ru>
2021-12-13 18:24:04 +0300

    hw7_2 - added output

--- hw_7/7_2.py
+++ hw_7/7_2.py
@@ -37,7 +37,16 @@ def qsort_inplace(lst, start=0, end=None):
 
 if __name__ == '__main__':
     seed(13)
-    some_list = [round(50 * float(random.random()), 1) for _ in range(50)]
+    some_list = [round(50 * float(random.random()), 1) for _ in range(20)]
     print(f'Start list: \n {some_list}')
     qsort_inplace(some_list)
     print(f'Sorted list: \n {some_list}')
+
+"""
+output: 
+Start list: 
+ [13.0, 34.3, 34.2, 42.5, 9.3, 11.5, 7.4, 11.3, 36.7, 6.5, 26.6, 10.7, 14.7, 21.6, 41.9, 30.4, 0.7, 13.8, 7.3, 43.6]
+Sorted list: 
+ [0.7, 6.5, 7.3, 7.4, 9.3, 10.7, 11.3, 11.5, 13.0, 13.8, 14.7, 21.6, 26.6, 30.4, 34.2, 34.3, 36.7, 41.9, 42.5, 43.6]
+
+"""
